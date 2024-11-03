8b0597c962c99845f97bda8ddfbf568c68345424
elr4dp7 <alexander.Chumaslov@mckesson.com>
2024-11-03 21:02:41 +0300

    Adjust hardhar.config

--- hardhat.config.cjs
+++ hardhat.config.cjs
@@ -1,6 +1,19 @@
 require('@nomicfoundation/hardhat-toolbox')
+require('hardhat-waffle')
+require('dotenv').config()
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: '0.8.27',
+  paths: {
+    artifacts: './artifacts',
+  },
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+    cardona: {
+      url: 'https://polygonzkevm-cardona.g.alchemy.com/v2/ss9270TQps48RQe2Yei2Ssj5j5vvwnmb',
+    },
+  },
 }
